7bcc66da64b732c9de8cf287e44ddc38358d957e
cicciorocca <francesco.roccamo@gmail.com>
2016-07-15 12:23:32 +0200

    Aggiunta SQLite

--- TestUWP/MainPage.xaml.vb
+++ TestUWP/MainPage.xaml.vb
@@ -20,26 +20,6 @@ Public NotInheritable Class MainPage
 
     Private Sub OpenPane(sender As Object, e As RoutedEventArgs)
         MainView.IsPaneOpen = True
-        'Dim l As List(Of ComuneCodCat) = New List(Of ComuneCodCat)()
-
-        'For Each elem In Vm.Comuni
-        '    l.Add(New ComuneCodCat() With {.nome = elem.nome, .codiceCatastale = elem.codiceCatastale})
-        'Next
-
-        'Dim serialized = JsonConvert.SerializeObject(l)
-
-        ''Dim newFile As Windows.Storage.StorageFile = Await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync("ComuniCodCat.json", Windows.Storage.CreationCollisionOption.ReplaceExisting)
-        ''Await Windows.Storage.FileIO.WriteTextAsync(newFile, serialized)
-
-        'Dim savePicker = New Windows.Storage.Pickers.FileSavePicker()
-        'savePicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop
-        'savePicker.FileTypeChoices.Add("Json", New List(Of String)() From {".json"})
-
-        'Dim newFile As Windows.Storage.StorageFile = Await savePicker.PickSaveFileAsync()
-        'If newFile IsNot Nothing Then
-        '    Await Windows.Storage.FileIO.WriteTextAsync(newFile, serialized)
-        'End If
-
     End Sub
 
     Private Sub ClosePane(sender As Object, e As RoutedEventArgs)
---
+++ TestUWP/Model/ListaViewModel.vb
@@ -0,0 +1,10 @@
+﻿Public Class ListaViewModel
+    Inherits ViewModelBase
+
+    Public ReadOnly Property VmName As String = "Lista"
+    Public Property Soggetto As ObservableCollection(Of SoggettoFiscale)
+
+    Public Overrides Function LoadViewModelAsync() As Task(Of Object)
+        Throw New NotImplementedException()
+    End Function
+End Class
